91007d6753af3bee904fe4ec3ab23325bf17113d
baturalp-kiziltan <baturalp.kiziltan@outlook.com>
2021-10-30 04:06:12 +0300

    initialize hw2

---
+++ 343/cmpe343_hw2_baturalp_kiziltan/src/Driver_Task1.java
@@ -0,0 +1,8 @@
+
+public class Driver_Task1 {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ 343/cmpe343_hw2_baturalp_kiziltan/src/Driver_Task2.java
@@ -0,0 +1,8 @@
+
+public class Driver_Task2 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
