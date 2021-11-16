e5de59b3fbc2ae56a0700e66a1fce203d20cc285
baturalp-kiziltan <baturalp.kiziltan@outlook.com>
2021-11-16 14:26:44 +0300

    Driver code, done!

--- 343/cmpe343_hw3_baturalp_kiziltan/src/Driver.java
+++ 343/cmpe343_hw3_baturalp_kiziltan/src/Driver.java
@@ -1,9 +1,66 @@
 
+import java.io.File;
+import java.util.*;
+
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		ProjectScheduler scheduler = null;
+		String filename = null;
+		
+		if (args.length <= 0) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the input filename: ");
+			filename = sc.nextLine().trim();
+			sc.close();
+		}
+		else {
+			filename = args[0];
+		}
+		
+		
+		try {
+			Scanner fileIO = new Scanner(new File(filename));
+			scheduler = new ProjectScheduler(fileIO);
+			fileIO.close();
+		} catch (Exception e) {
+			System.out.println("Failed to initialize Project Scheduler:");
+			System.out.println("\t- Check input source");
+			e.printStackTrace();
+		}
+		
+		Scanner stdIn = new Scanner(System.in);
+		while (true) {
+			System.out.println("Enter choice (0: Exit, 1: List schedule, 2: Check order): ");
+			String line = stdIn.nextLine().trim();
+			
+			int command = -1;
+			try {
+				command = Integer.parseInt(line);
+			} 
+			catch (NumberFormatException e) {
+				System.out.println("Invalid command. Type only numbers [0-2]");
+			}
+			
+			switch (command) {
+			case 0:
+				System.exit(0);;
+				break;
+			case 1:
+				scheduler.List();
+				break;
+			case 2:
+				System.out.print("Enter first task: ");
+				String task1 = stdIn.nextLine().trim();
+				System.out.print("Enter second task: ");
+				String task2 = stdIn.nextLine().trim();
+				scheduler.CheckOrder(task1, task2);
+				break;
+			default: System.out.println("Unknown command. Try something else.");
+			}
+		}
+		
 	}
 
 }
