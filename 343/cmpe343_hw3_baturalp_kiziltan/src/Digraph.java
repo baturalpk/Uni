c8ed3ab24aafa2d5b9b9bb69b34cacc37a01f7a8
baturalp-kiziltan <baturalp.kiziltan@outlook.com>
2021-11-16 12:04:42 +0300

    Digraph implementation completed

--- 343/cmpe343_hw3_baturalp_kiziltan/src/Digraph.java
+++ 343/cmpe343_hw3_baturalp_kiziltan/src/Digraph.java
@@ -1,4 +1,68 @@
 
+import java.util.*;
+
 public class Digraph {
+	
+    private final int V;
+    private int E;
+    private ArrayList<Integer>[] adj;
+    private int[] indegree;
+    
+    public Digraph(int V) {
+        if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be non-negative");
+        this.V = V;
+        this.E = 0;
+        indegree = new int[V];
+        adj = (ArrayList<Integer>[]) new ArrayList[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new ArrayList<Integer>();
+        }
+    }
+        
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    public void addEdge(int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
+        adj[v].add(w);
+        indegree[w]++;
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        validateVertex(v);
+        return adj[v];
+    }
+
+    public int outdegree(int v) {
+        validateVertex(v);
+        return adj[v].size();
+    }
+
+    public int indegree(int v) {
+        validateVertex(v);
+        return indegree[v];
+    }
+
+    public Digraph reverse() {
+        Digraph reverse = new Digraph(V);
+        for (int v = 0; v < V; v++) {
+            for (int w : adj(v)) {
+                reverse.addEdge(w, v);
+            }
+        }
+        return reverse;
+    }
 
 }
