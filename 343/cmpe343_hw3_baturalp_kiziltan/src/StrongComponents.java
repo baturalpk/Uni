9ab7ecf624c1532c6f8d93e0df0a8175a1be459f
baturalp-kiziltan <baturalp.kiziltan@outlook.com>
2021-11-16 12:28:30 +0300

    implementation of SCC done

--- 343/cmpe343_hw3_baturalp_kiziltan/src/StrongComponents.java
+++ 343/cmpe343_hw3_baturalp_kiziltan/src/StrongComponents.java
@@ -1,4 +1,50 @@
 
+// Makes use of Kosaraju & Sharir's strongly connected components algorithm
 public class StrongComponents {
+    private boolean[] marked;
+    private int[] id;
+    private int count;
+
+    public StrongComponents(Digraph G) {
+        DepthFirstOrder dfs = new DepthFirstOrder(G.reverse());
+        
+        marked = new boolean[G.V()];
+        id = new int[G.V()];
+        for (int v : dfs.reversePost()) {
+            if (!marked[v]) {
+                dfs(G, v);
+                count++;
+            }
+        }
+    }
+
+    private void dfs(Digraph G, int v) { 
+        marked[v] = true;
+        id[v] = count;
+        for (int w : G.adj(v)) {
+            if (!marked[w]) dfs(G, w);
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public boolean stronglyConnected(int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
+        return id[v] == id[w];
+    }
+    
+    public int id(int v) {
+        validateVertex(v);
+        return id[v];
+    }
+
+    private void validateVertex(int v) {
+        int V = marked.length;
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
 
 }
