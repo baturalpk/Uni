bd67242035b7b7f55118d2e4cc0d0880aeb308ed
baturalp-kiziltan <baturalp.kiziltan@outlook.com>
2021-11-16 12:13:36 +0300

    DFO implementation completed

--- 343/cmpe343_hw3_baturalp_kiziltan/src/DepthFirstOrder.java
+++ 343/cmpe343_hw3_baturalp_kiziltan/src/DepthFirstOrder.java
@@ -1,4 +1,67 @@
 
+import java.util.*;
+
 public class DepthFirstOrder {
+    private boolean[] marked;
+    private int[] pre;
+    private int[] post;
+    private Queue<Integer> preorder;
+    private Queue<Integer> postorder;
+    private int preCounter;
+    private int postCounter;
+
+    public DepthFirstOrder(Digraph G) {
+        pre    = new int[G.V()];
+        post   = new int[G.V()];
+        postorder = new LinkedList<Integer>();
+        preorder  = new LinkedList<Integer>();
+        marked    = new boolean[G.V()];
+        for (int v = 0; v < G.V(); v++)
+            if (!marked[v]) dfs(G, v);
+    }
+
+    private void dfs(Digraph G, int v) {
+        marked[v] = true;
+        pre[v] = preCounter++;
+        preorder.offer(v);
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                dfs(G, w);
+            }
+        }
+        postorder.offer(v);
+        post[v] = postCounter++;
+    }
+    
+    public int pre(int v) {
+        validateVertex(v);
+        return pre[v];
+    }
+
+    public int post(int v) {
+        validateVertex(v);
+        return post[v];
+    }
+
+    public Iterable<Integer> post() {
+        return postorder;
+    }
+
+    public Iterable<Integer> pre() {
+        return preorder;
+    }
+    
+    public Iterable<Integer> reversePost() {
+        Stack<Integer> reverse = new Stack<Integer>();
+        for (int v : postorder)
+            reverse.push(v);
+        return reverse;
+    }
+
+    private void validateVertex(int v) {
+        int V = marked.length;
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
 
 }
